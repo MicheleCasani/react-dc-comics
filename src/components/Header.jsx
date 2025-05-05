74cdadf73751cb11c2c73477e7ab895b8d43148a
MicheleCasani <casanimichele@gmail.com>
2025-05-05 15:40:32 +0200

    [UPD] add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header>
-            <div>Ciao</div>
-        </header>
+        <div className='container'>
+            <header>
+                <img src="../assets/dc-logo.png" alt="" />
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </header>
+        </div>
     )
 }
 
 export default Header
+
